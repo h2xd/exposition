bfb3db9354ccae1d16686477d9f1004c58b8d43f
Andrej Schujkow <a.schujkow@gmail.com>
2022-06-08 18:57:27 +0200

    feat: add msw integration

--- packages/msw/functions/defineMSWIntegration.ts
+++ packages/msw/functions/defineMSWIntegration.ts
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import type { RequestHandler, SetupWorkerApi } from 'msw'
 import type { SetupServerApi } from 'msw/node'
+=======
+import type { DefaultBodyType, MockedRequest, RestHandler, setupWorker } from 'msw'
+import type { setupServer } from 'msw/node'
+>>>>>>> feat: add msw integration
 import type { Exposition, ExpositionConfig, ExpositionValues } from '@exposition/core'
 import { getExpositionValues } from '@exposition/core'
 
