d0d3a750312386ad79f6999fcd1cd5213868e0d6
Andrej Schujkow <19648055+h2xd@users.noreply.github.com>
2022-07-21 20:32:13 +0200

    feat(core): add builder class (#39)
    * create Exposition class
    * add integration support
    * add `update`, `reset` and `initialized` hooks
    * refactor msw integration to use the new class pattern

---
+++ packages/integrations/msw/functions/createMswIntegration.spec.ts
@@ -0,0 +1,61 @@
+import { Exposition } from '@exposition/core'
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+import { afterEach, describe } from 'vitest'
+import fetch from 'node-fetch'
+import { createMswIntegration } from './createMswIntegration'
+
+describe('createMswIntegration', () => {
+  const exposition = new Exposition({
+    user: {
+      options: ['authorized', 'unauthorized'],
+    },
+  } as const)
+
+  beforeAll(() => {
+    const mswIntegration = exposition.use(createMswIntegration<typeof exposition>, { msw: setupServer() })
+
+    mswIntegration.createHandler((values) => {
+      return [
+        rest.get('https://api.example.com/account', (_request, response, context) => {
+          switch (values.user) {
+            case 'authorized':
+              return response(context.json({ name: 'Spanky 🐕' }))
+            case 'unauthorized':
+              return response(context.status(401))
+          }
+        }),
+      ]
+    })
+
+    exposition.init()
+  })
+
+  afterEach(() => { exposition.reset() })
+
+  it('should return an authorized code by default', async () => {
+    const response = await fetch('https://api.example.com/account')
+
+    expect(response.status).toBe(200)
+  })
+
+  it('should return an 401 when set to unauthorized', async () => {
+    exposition.update({ user: 'unauthorized' })
+
+    const response = await fetch('https://api.example.com/account')
+
+    expect(response.status).toBe(401)
+  })
+
+  // it('should use no handler if explicitly called', async () => {
+  //   await integration.useNoHandlers()
+
+  //   try {
+  //     await fetch('/')
+  //   }
+  //   catch (error) {
+  //     expect(error).toBeDefined()
+  //   }
+  // })
+})
+
