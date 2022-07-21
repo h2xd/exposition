9878617854f92cc27ebcc100f23373591bc9d280
Andrej Schujkow <a.schujkow@gmail.com>
2022-07-22 00:14:53 +0200

    chore(playground): update vue-devtools setup

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,25 +1,17 @@
+// #region vue-devtools
 import { createApp } from 'vue'
-// import { setupDevtools } from '@exposition/integrations/vue-devtools'
+import { setupDevtools } from '@exposition/integrations/vue-devtools'
 import App from './App.vue'
 
 import { playgroundExposition } from './utils/exposition'
 
 const app = createApp(App)
 
-// #region vue-devtools
+app.use(setupDevtools, {
+  exposition: playgroundExposition,
+})
+
 playgroundExposition.init()
-
-// app.use(setupDevtools, {
-//   exposition: playgroundExposition,
-//   onUpdate(newValues: any, isEnabled: boolean) {
-//     if (!isEnabled) {
-//       mswIntegration.useNoHandlers()
-//       return
-//     }
-
-//     mswIntegration.updateValues(newValues)
-//   },
-// })
 
 app.mount('#app')
 // #endregion vue-devtools
