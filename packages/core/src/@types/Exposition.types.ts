db0bf5f065095b0db318f46672ee5226e906aa39
Andrej Schujkow <a.schujkow@gmail.com>
2022-07-23 17:32:00 +0200

    docs(core): add TSCoc and examples to the README
    - Add TSDoc to each method
    - Add README for the package
    - Add TSDoc to settings
    - Update getting started
    
    closes #52

--- packages/core/src/@types/Exposition.types.ts
+++ packages/core/src/@types/Exposition.types.ts
@@ -1,5 +1,15 @@
 export interface ExpositionSettings {
+  /**
+   * Signal all integrations that they should stop their actions
+   *
+   * - [`@exposition/integrations/msw`](https://h2xd.github.io/exposition/integrations/msw.html) will reset all handler if this option is set to `false`
+   */
   active: boolean
+  /**
+   * Signal all integrations that they should prevent all state restoration handler
+   *
+   * - [`@exposition/integrations/vue-devtools`](https://h2xd.github.io/exposition/integrations/vue-devtools.html) will not interact with the `localStorage` if this option is set to `false`
+   */
   restoreState: boolean
 }
 
